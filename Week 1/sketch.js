f901867ea6a888f46b7beac97dcd838f27b12e0b
Patrick Prentice <pxp387@miami.edu>
2018-08-22 17:49:45 -0400

    Patrick Prentice (CIM540)

---
+++ Week 1/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800, 400);
+  background(204);
+}
+
+function draw() {
+  ellipse(150, 100, 200, 100); //bigcircle
+  ellipse(150, 120, 100, 22); //mouth
+  ellipse(100, 40, 30, 30); //lefteye
+  ellipse(200, 40, 30, 30); //righteye
+}
